70c55a6d49184fca656b15e98cb7f92dfb95354a
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2022-06-11 02:08:38 -0400

    cleaned up stuff and ensured a build is possible

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,13 @@ export default function Home() {
       <title>replit</title>
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" /> 
       </Head>
 
       <h1>ArtistBubble</h1>
       <p>View your Bubble Today!</p>
-      <a class="button scaleIn" id="loginButton" href={`https://accounts.spotify.com/authorize?client_id=57e3f0d0690c485884d4088dd9fef246&redirect_uri=${baseURL}/callback&scope=playlist-read-collaborative%20user-library-read%20user-top-read&response_type=token&show_dialog=true`}>Login with Spotify</a>
+      <a className={"button scaleIn"} id="loginButton" href={`https://accounts.spotify.com/authorize?client_id=57e3f0d0690c485884d4088dd9fef246&redirect_uri=${baseURL}/callback&scope=playlist-read-collaborative%20user-library-read%20user-top-read&response_type=token&show_dialog=true`}>Login with Spotify</a>
       <br/>
       <p>Made by <a href="https://piemadd.com">Piero</a>. pls hire me lol (dev and comm manager)</p>
     </>
