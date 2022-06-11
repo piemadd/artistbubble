86731decdc971e87f97b1ce5af4493f204fdad0f
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2022-06-11 02:11:46 -0400

    fixed title

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Head>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width" />
-      <title>replit</title>
+      <title>chart?</title>
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
