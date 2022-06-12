187ba71acad942860910093126cf075ec3f78e3a
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2022-06-12 01:10:55 -0400

    fixed tagline

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
       </Head>
 
       <h1>SpotifyGrid</h1>
-      <p>View your Bubble Today!</p>
+      <p>View your Grid Today!</p>
       <a className={"button scaleIn"} id="loginButton" href={`https://accounts.spotify.com/authorize?client_id=57e3f0d0690c485884d4088dd9fef246&redirect_uri=${baseURL}/callback&scope=playlist-read-collaborative%20user-library-read%20user-top-read&response_type=token&show_dialog=true`}>Login with Spotify</a>
       <br/>
       <p>Made by <a href="https://piemadd.com">Piero</a>. pls hire me lol (dev and comm manager)</p>
